285006e4cc262b54cd82e38fba9f55f723cb692b
Simon Ensslen <sensslen@gmail.com>
2023-07-25 21:50:48 +0200

    Don't use the streamed_at date but the pubiished date, as it appears that the streamed_at date is not set sometimes. Also don't rely on the event count, as there is a maximum of items served by the livestream server. Rather use the api used by the website to gather the information needed

--- cgf.livestream.download.ps1
+++ cgf.livestream.download.ps1
@@ -11,87 +11,88 @@ param(
 
 $output_folder_full = [IO.Path]::GetFullPath($output_folder)
 if (!(Test-Path -Path $output_folder_full -PathType Container)) {
-    Write-Error "Could not find output folder $output_folder_full"
+    Write-Error "Could not find output folder ${output_folder_full}"
     exit
 }
 
 $ffmpeg_location_full = [IO.Path]::GetFullPath($ffmpeg_location)
 if (!(Test-Path -Path $ffmpeg_location_full  -PathType Leaf)) {
-    Write-Error "Could not find ffmpeg at $ffmpeg_location_full"
+    Write-Error "Could not find ffmpeg at ${ffmpeg_location_full}"
     exit
 }
 
-$eventInfoUrl = "https://api.new.livestream.com/accounts/$account_id/events/$event_id/"
-Write-Output "Downloading Event Information using url:$eventInfoUrl"
+function Get-ChunkVideoInfo {
+    param(
+        [string]$chunkUrl
+    )
 
-$eventInfoResponse = Invoke-WebRequest $eventInfoUrl
+    Write-Host "Downloading Event feed chunk using url:$feedChunkUrl"
+    $feedChunkResponse = Invoke-WebRequest $feedChunkUrl
+    if ($feedChunkResponse.StatusCode -ne [system.net.httpstatuscode]::ok) {
+        Write-Error "Failed to download feed information"
+        Write-Error $feedChunkResponse
+        exit
+    }    
+    $feedChunk = $feedChunkResponse.Content | ConvertFrom-Json
 
-if ($eventInfoResponse.StatusCode -ne [system.net.httpstatuscode]::ok) {
-    Write-Error "Failed to gather event information"
-    Write-Error $eventInfoResponse
-    exit
+    return $feedChunk.data
 }
 
-$eventInfo = $eventInfoResponse.Content | ConvertFrom-Json
+$chunkSize = 10
+$feedChunkUrl = "https://api.new.livestream.com/accounts/${account_id}/events/${event_id}/feed/?maxItems=${chunkSize}"
 
-$numberOfFilesInFeed = $eventInfo.feed.total
+$videos = Get-ChunkVideoInfo($feedChunkUrl)
 
-$completeFeedUrl = "https://api.new.livestream.com/accounts/$account_id/events/$event_id/feed/?maxItems=$numberOfFilesInFeed"
-Write-Output "Downloading Event feed using url:$completeFeedUrl"
+while ($videos.Count -gt 0) {
 
-$completeFeedResponse = Invoke-WebRequest $completeFeedUrl
-
-if ($completeFeedResponse.StatusCode -ne [system.net.httpstatuscode]::ok) {
-    Write-Error "Failed to download feed information"
-    Write-Error $completeFeedResponse
-    exit
-}
-
-$completeFeed = $completeFeedResponse.Content | ConvertFrom-Json
-
-$videos = $completeFeed.data
-
-Write-Host "Downloading all $numberOfFilesInFeed videos from event with id:$event_id"
-
-foreach ($video in $videos) {
-    $videoName = $video.data.caption
-    $videoCreationDate = [Datetime]::Parse($video.data.created_at)
-    $videoUploadDate = [Datetime]::Parse($video.data.streamed_at)
-    $datePrefix = $videoUploadDate.ToString("yyyy-MM-dd-")
-    $videoNameWithoutInvalidCharacters = $videoName.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
-    $fileName = "$datePrefix$videoNameWithoutInvalidCharacters.m4v"
-    $filePath = [IO.Path]::Combine($output_folder_full, $fileName)
-
-    Write-Host "Downloading video to $filePath"
-
-    if (Test-Path -Path $filePath -PathType Any) {
-        Write-Output "Skipping the download of $filePath, as the file already exists"
-    }
-    else {
+    foreach ($video in $videos) {
         $video_id = $video.data.id
-        $videoInfoUrl = "https://api.new.livestream.com/accounts/$account_id/events/$event_id/videos/$video_id"
-        Write-Output "Downloading Video information using:$videoInfoUrl"
+        $videoInfoUrl = "https://api.new.livestream.com/accounts/${account_id}/events/${event_id}/videos/${video_id}"
 
-        $videoInfoResponse = Invoke-WebRequest $videoInfoUrl
+        Write-Host ""
+        Write-Host ""
+        Write-Host "Starting download of Video using:$videoInfoUrl"
 
-        if ($videoInfoResponse.StatusCode -ne [system.net.httpstatuscode]::ok) {
-            Write-Error "Failed to get video information via: $videoInfoUrl"
-            Write-Error $videoInfoResponse
-            exit
+        $videoName = $video.data.caption
+        $videoCreationDate = [DateTime]::Parse($video.data.created_at)
+        $videoPulishDate = [DateTime]::Parse($video.data.publish_at)
+        $datePrefix = $videoPulishDate.ToString("yyyy-MM-dd-")
+        $videoNameWithoutInvalidCharacters = $videoName.Split([IO.Path]::GetInvalidFileNameChars()) -join '_'
+        $fileName = "${datePrefix}${videoNameWithoutInvalidCharacters}.m4v"
+        $filePath = [IO.Path]::Combine($output_folder_full, $fileName)
+
+        Write-Host "Downloading video to ""${filePath}"""
+
+        if (Test-Path -Path $filePath -PathType Any) {
+            Write-Host "Skipping the download. The File ""${filePath}"" already exists"
         }
+        else {
+            $videoInfoResponse = Invoke-WebRequest $videoInfoUrl
 
-        $videoInfo = $videoInfoResponse.Content | ConvertFrom-Json
+            if ($videoInfoResponse.StatusCode -ne [system.net.httpstatuscode]::ok) {
+                Write-Error "Failed to get video information via: ${videoInfoUrl}"
+                Write-Error $videoInfoResponse
+                exit
+            }
 
-        $maximumVideoBitrate = ($videoInfo.asset.qualities | ForEach-Object { $_.bitrate } | Measure-Object -Maximum).Maximum
+            $videoInfo = $videoInfoResponse.Content | ConvertFrom-Json
 
-        $downloadUrl = $videoInfo.m3u8_url
-        $ffmpegCommand = """$ffmpeg_location_full"" -i ""$downloadUrl"" -metadata title=""$videoName"" -map m:variant_bitrate:$maximumVideoBitrate -c copy ""$filePath"""
-        Write-Output "Executing $ffmpegCommand"
+            $maximumVideoBitrate = ($videoInfo.asset.qualities | ForEach-Object { $_.bitrate } | Measure-Object -Maximum).Maximum
 
-        & $ffmpeg_location_full -i """$downloadUrl""" -metadata "title=""$videoName""" -map "m:variant_bitrate:$maximumVideoBitrate" -bsf:a "aac_adtstoasc" -c "copy" """$filePath"""
+            $downloadUrl = $videoInfo.m3u8_url
+            $ffmpegCommand = """${ffmpeg_location_full}"" -i ""${downloadUrl}"" -metadata title=""${videoName}"" -map m:variant_bitrate:${maximumVideoBitrate} -c copy ""${filePath}"""
+            Write-Host "Executing $ffmpegCommand"
 
-        Write-Output "Setting creation time of file:$filePath to:$videoCreationDate"
-        $(Get-Item $filePath).CreationTime = $videoCreationDate
-        $(Get-Item $filePath).LastWriteTime = $videoUploadDate
+            & $ffmpeg_location_full -i """${downloadUrl}""" -t "5" -metadata "title=""${videoName}""" -map "m:variant_bitrate:${maximumVideoBitrate}" -bsf:a "aac_adtstoasc" -c "copy" """${filePath}"""
+
+            Write-Host "Setting creation time of file:${filePath} to:${videoCreationDate}"
+            $(Get-Item $filePath).CreationTime = $videoCreationDate
+            Write-Host "Setting las write time of file:${filePath} to:${videoPulishDate}"
+            $(Get-Item $filePath).LastWriteTime = $videoPulishDate
+        }
     }
+
+    $lastVideoId = $videos[-1].data.id
+    $feedChunkUrl = "https://api.new.livestream.com/accounts/${account_id}/events/${event_id}/feed/?id=${lastVideoId}&type=video&newer=-1&older=${chunkSize}"
+    $videos = Get-ChunkVideoInfo($feedChunkUrl)
 }
